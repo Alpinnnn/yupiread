729b037cf61dde8c0575f071c09e188c20eccc47
Alpinnnn <alvinchristianfarrell@gmail.com>
2025-09-22 19:10:47 +0700

    Build Error Fix 2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,10 +11,9 @@ android {
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-        // Flag to enable support for the new language APIs
-        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
     
     // Suppress Java 8 obsolete warnings
@@ -56,6 +55,5 @@ flutter {
 }
 
 dependencies {
-    // Core library desugaring for Java 8+ API support on older Android versions
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
